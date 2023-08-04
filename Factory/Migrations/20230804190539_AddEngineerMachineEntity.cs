6428bf939d15c12f6450281bae531c8f2aac5491
ellehailu <hailu.elle@gmail.com>
2023-08-04 12:07:29 -0700

    set up migration and create tables in db

---
+++ Factory/Migrations/20230804190539_AddEngineerMachineEntity.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Factory.Migrations
+{
+    public partial class AddEngineerMachineEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EngineerMachines",
+                columns: table => new
+                {
+                    EngineerMachineId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    EngineerId = table.Column<int>(type: "int", nullable: false),
+                    MachineId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EngineerMachines", x => x.EngineerMachineId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EngineerMachines");
+        }
+    }
+}
--- Factory/Models/FactoryContext.cs
+++ Factory/Models/FactoryContext.cs
@@ -8,6 +8,8 @@ namespace Factory.Models
     {
         public DbSet<Engineer> Engineers { get; set; }
         public DbSet<Machine> Machines { get; set; }
+
+        public DbSet<EngineerMachine> EngineerMachines { get; set; }
         public FactoryContext(DbContextOptions options) : base(options) { }
     }
 }

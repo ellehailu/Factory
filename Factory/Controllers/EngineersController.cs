d5ef2968415bb9dcfc671a8a74a82a59a4f92fde
ellehailu <hailu.elle@gmail.com>
2023-08-05 13:20:04 -0700

    add details view and route for engineer class

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -31,5 +31,11 @@ namespace Factory.Controllers
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        public ActionResult Details(int id)
+        {
+            Engineer thisEngineer = _db.Engineers.FirstOrDefault(engineer => engineer.EngineerId == id);
+            return View(thisEngineer);
+        }
     }
 }

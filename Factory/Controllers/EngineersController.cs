deda2eee93511bf7678448f6cc7e74c9396e3d2f
ellehailu <hailu.elle@gmail.com>
2023-08-05 17:02:55 -0700

    update action links to correctly target ids for edit and delete features

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -1,6 +1,5 @@
-using Microsoft.AspNetCore.Mvc;
 using Factory.Models;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 
 namespace Factory.Controllers
@@ -38,6 +37,7 @@ namespace Factory.Controllers
             return View(thisEngineer);
         }
 
+
         public ActionResult Edit(int id)
         {
             Engineer thisEngineer = _db.Engineers.FirstOrDefault(engineer => engineer.EngineerId == id);
@@ -51,5 +51,25 @@ namespace Factory.Controllers
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
+
+
+        public ActionResult Delete(int id)
+        {
+            Engineer thisEngineer = _db.Engineers.FirstOrDefault(engineer => engineer.EngineerId == id);
+            return View(thisEngineer);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmation(int id)
+        {
+            Engineer thisEngineer = _db.Engineers.FirstOrDefault(engineer => engineer.EngineerId == id);
+            _db.Engineers.Remove(thisEngineer);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
+
+
+
+    
